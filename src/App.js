46d20d17a388c12cca27b7d819c494192526a6d6
codewithrich <richardson12230@gmail.com>
2024-12-11 15:20:12 +0530

    Infinite  Scrolling image slider complete

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import WorksImageSlider from './WorksImageSlider';
+
+function App() {
+  return <>
+  <div>
+    <h1 style={{textAlign:"center"}}>Image Slider App 1</h1>
+  </div>
+  <WorksImageSlider/>
+  </>;
+}
+
+export default App;
---
+++ src/WorksImageSlider.js
@@ -0,0 +1,114 @@
+import React from "react";
+import './WorksImageSlider.css';
+import image1 from './assets/testImages/Image1.jpg';
+import image2 from './assets/testImages/image2.jpg';
+import image3 from './assets/testImages/image3.jpg';
+import image4 from './assets/testImages/image4.jpg';
+import image5 from './assets/testImages/image5.jpg';
+import image6 from './assets/testImages/image6.jpg';
+
+
+const WorksImageSlider = () => {
+  return (
+    <>
+    <section id="WorksImageSlider">
+      <div class="slider">
+        <div class="slide-track">
+          <div class="slide">
+            <img className="Sliderimages" src={image1} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image2} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image3} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image4} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image5} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image6} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image1} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image2} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image3} alt="slidingInternshipImage"/>
+          </div>
+
+
+          <div class="slide">
+            <img className="Sliderimages" src={image1} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image2} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image3} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image4} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image5} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image6} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image1} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image2} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image3} alt="slidingInternshipImage"/>
+          </div>
+
+
+          <div class="slide">
+            <img className="Sliderimages" src={image1} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image2} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image3} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image4} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image5} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image6} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image1} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image2} alt="slidingInternshipImage"/>
+          </div>
+          <div class="slide">
+            <img className="Sliderimages" src={image3} alt="slidingInternshipImage"/>
+          </div>
+
+
+          
+
+         
+        </div>
+      </div>
+      </section>
+    </>
+  );
+};
+
+export default WorksImageSlider;
